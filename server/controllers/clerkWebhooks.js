f7e8af78eff1daa9bc84dbc8268671cbe0aaf988
yvishi <y.v.s.vishnoi@gmail.com>
2025-12-31 00:48:42 +0530

    clerk setup completed

---
+++ server/controllers/clerkWebhooks.js
@@ -0,0 +1,51 @@
+import user from "../models/user.js";
+import { Webhook } from "svix";
+
+
+const clerkWebhooks=async(req,res)=>{
+    try{
+        //svix instance with clerk webhook secret
+        const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+        
+        const headers={
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"],
+        };
+
+        await whook.verify(JSON.stringify(req.body),headers);
+
+        const {data,type}=req.body;
+        const userData={
+            _id: data.id,
+            username: data.first_name + " " + data.last_name,
+            email: data.email_adresses[0].email_address,
+            image: data.image_url,        
+            }
+            switch (type) {
+                case "user.created":{
+                    await user.create(userData);
+                    break;
+                }
+                case "user.updated":{
+                    await user.findByIdAndUpdate(data.id, userData);
+                    break;
+                }
+                case "user.deletd":{
+                    await user.findByIdAndDelete(data.id);
+                    break;
+                }
+                    
+                    
+                default:
+                    break;
+            }
+            res.json({success:true,message:"Webhook received"});
+       
+    }catch(e){
+        console.log(e.message);
+        res.json({success:false,message:e.message});
+    }
+}
+
+export default clerkWebhooks;
--- server/server.js
+++ server/server.js
@@ -3,12 +3,22 @@ import express from "express"
 import "dotenv/config"
 import cors from "cors"
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB();
+
 const app=express();
 const port=process.env.PORT || 3000;
+
 app.use(cors());
 
+//Middleware
+app.use(express.json());
+app.use(clerkMiddleware());
+
+//API for ClerkWebhook
+app.use("/api/clerk", clerkWebhooks);
 
 app.get("/",(req,res)=>{
     res.send("API Working!!")
