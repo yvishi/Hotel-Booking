7218860442f78bf1c99bfa7297ca34bc992d7bac
yvishi <y.v.s.vishnoi@gmail.com>
2026-01-06 00:01:21 +0530

    fix2

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB=async()=>{
     try{
+        console.log("MONGO_URI:", process.env.MONGODB_URI);
+
         // mongoose.connection.on('connected',()=>console.log("Database connected"));
         await mongoose.connect(`${process.env.MONGODB_URI}/hotelBooking`);
         console.log("Database connected");
